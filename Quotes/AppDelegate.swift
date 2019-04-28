75bdcef290b94dc220b1fd639ff709152d0a01b5
zuzhi <476513451@qq.com>
2019-04-28 21:28:50 +0800

    Initial Commit

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by zuzhi hu on 2019/4/28.
+//  Copyright © 2019 zuzhi hu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Quotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by zuzhi hu on 2019/4/28.
+//  Copyright © 2019 zuzhi hu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
